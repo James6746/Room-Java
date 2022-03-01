09143f879f46ba1ce7019589274a4f9a9104789c
James <sobirovjamshid18@gmail.com>
2022-03-01 09:37:59 +0500

    second commit

--- app/src/main/java/com/example/roomjava/MainActivity.java
+++ app/src/main/java/com/example/roomjava/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.roomjava;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.roomjava.database.UserRepository;
 import com.example.roomjava.model.User;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     Button button;
+    TextView tv_db_size;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
         button = findViewById(R.id.btn_press);
+        tv_db_size = findViewById(R.id.tv_db_size);
 
         button.setOnClickListener(it -> {
             roomDatabase();
@@ -30,7 +36,27 @@ public class MainActivity extends AppCompatActivity {
     private void roomDatabase() {
         UserRepository repository = new UserRepository(getApplication());
         User user = new User(1, "Sobirov Jamshid");
-        repository.saveUser(user);
-//        new UserASyncTask(repository).execute(user);
+//        repository.saveUser(user);
+        new UserAsyncTask(repository).execute(user);
+    }
+
+    class UserAsyncTask extends AsyncTask<User, Void, List<User>> {
+        UserRepository repository;
+
+        UserAsyncTask(UserRepository repository) {
+            this.repository = repository;
+        }
+
+        @Override
+        protected List<User> doInBackground(User... users) {
+            repository.saveUser(users[0]);
+            return repository.getUsers();
+        }
+
+        @Override
+        protected void onPostExecute(List<User> users) {
+            super.onPostExecute(users);
+            tv_db_size.setText("Room DB size is " + users.size());
+        }
     }
 }
